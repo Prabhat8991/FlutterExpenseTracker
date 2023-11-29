ecb75147f6b82d39bcfebaa4aed7b5442c413817
Prabhat8991 <singh.prabhat8991@gmail.com>
2023-11-29 17:55:16 +0530

    added datepicker and amount field

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -11,16 +11,27 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleTextController = TextEditingController();
+  final _amountTextController = TextEditingController();
+
+
+  void _showDatePicker(BuildContext context) {
+    DateTime now = DateTime.now();
+    DateTime firstDate = DateTime(now.year-1);
+    DateTime lastDate = now;
+
+    showDatePicker(context: context, initialDate: now, firstDate: firstDate, lastDate: lastDate);
+  }
 
   @override
   void dispose() {
     _titleTextController.dispose();
+    _amountTextController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
@@ -29,11 +40,47 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text("Title")),
           ),
+          const SizedBox(
+            height: 20,
+          ),
           Row(children: [
-            ElevatedButton(onPressed: () {
-              print(_titleTextController.text);
-            }, child: const Text("Save"))
-          ],)
+            Expanded(
+                child: TextField(
+              keyboardType: TextInputType.number,
+              controller: _amountTextController,
+              decoration: const InputDecoration(
+                  prefixText: '\$', label: Text("Amount")),
+            )),
+            Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text("Select Date"),
+                IconButton(
+                  onPressed: () {
+                    _showDatePicker(context);
+                  },
+                  icon: const Icon(Icons.calendar_month),
+                )
+              ],
+            ))
+          ]),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleTextController.text);
+                    print(_amountTextController.text);
+                  },
+                  child: const Text("Save")),
+              const SizedBox(width: 20),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel"))
+            ],
+          )
         ],
       ),
     );
