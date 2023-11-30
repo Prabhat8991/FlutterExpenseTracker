6be38d226f242ed277b706b83e41516c4ad1289f
Prabhat8991 <singh.prabhat8991@gmail.com>
2023-11-30 09:26:12 +0530

    added drop down

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -14,6 +14,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleTextController = TextEditingController();
   final _amountTextController = TextEditingController();
   DateTime? _selectedDateTime;
+  var _selectedCategory = Category.leisure;
 
   void _showDatePicker(BuildContext context) async {
     DateTime now = DateTime.now();
@@ -40,59 +41,77 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
           TextField(
-            controller: _titleTextController,
-            maxLength: 50,
-            decoration: const InputDecoration(label: Text("Title")),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(children: [
-            Expanded(
-                child: TextField(
-              keyboardType: TextInputType.number,
-              controller: _amountTextController,
-              decoration: const InputDecoration(
-                  prefixText: '\$', label: Text("Amount")),
-            )),
-            Expanded(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text((_selectedDateTime == null)
-                    ? "No date selected"
-                    : formatter.format(_selectedDateTime!)),
-                IconButton(
-                  onPressed: () {
-                    _showDatePicker(context);
-                  },
-                  icon: const Icon(Icons.calendar_month),
-                )
-              ],
-            ))
-          ]),
-          Row(
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    print(_titleTextController.text);
-                    print(_amountTextController.text);
-                  },
-                  child: const Text("Save")),
-              const SizedBox(width: 20),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Cancel"))
-            ],
-          )
-        ],
-      ),
+          controller: _titleTextController,
+          maxLength: 50,
+          decoration: const InputDecoration(label: Text("Title")),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(children: [
+          Expanded(
+              child: TextField(
+                keyboardType: TextInputType.number,
+                controller: _amountTextController,
+                decoration: const InputDecoration(
+                    prefixText: '\$', label: Text("Amount")),
+              )),
+          Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text((_selectedDateTime == null)
+                      ? "No date selected"
+                      : formatter.format(_selectedDateTime!)),
+                  IconButton(
+                    onPressed: () {
+                      _showDatePicker(context);
+                    },
+                    icon: const Icon(Icons.calendar_month),
+                  )
+                ],
+              ))
+        ]),
+        const SizedBox(height: 20,),
+        Row(
+        children: [
+        DropdownButton(
+        value: _selectedCategory,
+        items: Category.values
+            .map((category) =>
+            DropdownMenuItem(
+                value: category,
+                child: Text(category.name.toUpperCase())))
+            .toList(),
+        onChanged: (value) {
+          if (value == null) {
+            return;
+          }
+          setState(() {
+            _selectedCategory = value;
+          });
+        }),
+    const Spacer(),
+    ElevatedButton(
+    onPressed: () {
+    print(_titleTextController.text);
+    print(_amountTextController.text);
+    },
+    child: const Text("Save")),
+    const SizedBox(width: 20),
+    ElevatedButton(
+    onPressed: () {
+    Navigator.pop(context);
+    },
+    child: const Text("Cancel"))
+    ],
+    )
+    ],
+    ),
     );
   }
 }
