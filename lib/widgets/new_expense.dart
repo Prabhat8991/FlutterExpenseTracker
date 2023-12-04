16c86982bb36baff8ac1ed4f1e3317de890a7fad
Prabhat8991 <singh.prabhat8991@gmail.com>
2023-12-04 18:21:57 +0530

    handled keyboard overlay

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -22,29 +22,28 @@ class _NewExpenseState extends State<NewExpense> {
     var amountEntered = double.tryParse(_amountTextController.text.trim());
     var isAmountInValid = amountEntered == null;
 
-    if (_titleTextController.text
-        .trim()
-        .isEmpty || isAmountInValid) {
-      showDialog(context: context, builder: (context) =>
-          AlertDialog(
-            title: const Text("Invalid Values"),
-            content: const Text("Please enter valid amount and title"),
-            actions: [
-              TextButton(onPressed: () {
-                Navigator.pop(context);
-              }, child: const Text("Ok"))
-            ],
-
-          ));
+    if (_titleTextController.text.trim().isEmpty || isAmountInValid) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text("Invalid Values"),
+                content: const Text("Please enter valid amount and title"),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Ok"))
+                ],
+              ));
       return;
     }
 
-    widget.onNewExpense(
-        Expense(title: _titleTextController.text,
-            amount: amountEntered,
-            date: _selectedDateTime!,
-            category: _selectedCategory)
-    );
+    widget.onNewExpense(Expense(
+        title: _titleTextController.text,
+        amount: amountEntered,
+        date: _selectedDateTime!,
+        category: _selectedCategory));
     Navigator.pop(context);
   }
 
@@ -72,28 +71,32 @@ class _NewExpenseState extends State<NewExpense> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 20, 16, 16),
-      child: Column(
-        children: [
-          TextField(
-            controller: _titleTextController,
-            maxLength: 50,
-            decoration: const InputDecoration(label: Text("Title")),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(children: [
-            Expanded(
-                child: TextField(
+    final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
+    return SizedBox(
+        height: double.infinity,
+        child: SingleChildScrollView(
+            child: Padding(
+          padding: EdgeInsets.fromLTRB(16, 20, 16, keyboardSpace + 16),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleTextController,
+                maxLength: 50,
+                decoration: const InputDecoration(label: Text("Title")),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(children: [
+                Expanded(
+                    child: TextField(
                   keyboardType: TextInputType.number,
                   controller: _amountTextController,
                   decoration: const InputDecoration(
                       prefixText: '\$', label: Text("Amount")),
                 )),
-            Expanded(
-                child: Row(
+                Expanded(
+                    child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Text((_selectedDateTime == null)
@@ -107,42 +110,43 @@ class _NewExpenseState extends State<NewExpense> {
                     )
                   ],
                 ))
-          ]),
-          const SizedBox(height: 20,),
-          Row(
-            children: [
-              DropdownButton(
-                  value: _selectedCategory,
-                  items: Category.values
-                      .map((category) =>
-                      DropdownMenuItem(
-                          value: category,
-                          child: Text(category.name.toUpperCase())))
-                      .toList(),
-                  onChanged: (value) {
-                    if (value == null) {
-                      return;
-                    }
-                    setState(() {
-                      _selectedCategory = value;
-                    });
-                  }),
-              const Spacer(),
-              ElevatedButton(
-                  onPressed: () {
-                    _validateSubmitData();
-                  },
-                  child: const Text("Save")),
-              const SizedBox(width: 20),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Cancel"))
+              ]),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  DropdownButton(
+                      value: _selectedCategory,
+                      items: Category.values
+                          .map((category) => DropdownMenuItem(
+                              value: category,
+                              child: Text(category.name.toUpperCase())))
+                          .toList(),
+                      onChanged: (value) {
+                        if (value == null) {
+                          return;
+                        }
+                        setState(() {
+                          _selectedCategory = value;
+                        });
+                      }),
+                  const Spacer(),
+                  ElevatedButton(
+                      onPressed: () {
+                        _validateSubmitData();
+                      },
+                      child: const Text("Save")),
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Cancel"))
+                ],
+              )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        )));
   }
 }
